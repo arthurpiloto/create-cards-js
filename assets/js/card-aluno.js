71ce0e8eb86581e684a20041cccf67b32fcfa725
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-10-17 08:41:54 -0300

    criando componentes e aplicando css

--- assets/js/card-aluno.js
+++ assets/js/card-aluno.js
@@ -1,16 +1,34 @@
 // SEMPRE QUE FORMOS CRIAR UM COMPONENTE É OBRIGATÓRIO USAR ESSA ESTRUTURA
-class card extends HTMLElement {
+class Card extends HTMLElement {
     constructor() {
         super()
         this.shadow = this.attachShadow({mode: `open`})
     }
 
+    // SEMELHANTE A CLASSE CONSTRUTORA DO KOTLIN (onCreate) - SEMPRE QUE A CLASSE Card FOR CHAMADA A PRIMEIRA COISA PUXADA PARA EXECUÇÃO DEPOIS DO constructor() SERÁ ELA
     connectedCallback() {
         this.shadow.appendChild(this.component())
+        this.shadow.appendChild(this.styles())
     }
 
+    // CRIA UM "style"
+    styles() {
+        const style = document.createElement(`style`)
+        style.textContent = `
+            .card {
+                margin: 0;
+                width: 400px;
+                height: 400px;
+                background-color: tomato;
+            }
+        `
+        return style
+    }
+
+    // CRIA O COMPONENTE card
     component() {
         const card = document.createElement(`div`)
+        card.classList.add(`card`)
         card.innerHTML = `
             <div>Arthur</div>
             <div></div>
@@ -21,4 +39,4 @@ class card extends HTMLElement {
 }
 
 // DEFINE UMA NOVA TAG NO HTML. 1º PARÂMETRO = NOME DA TAG E 2º PARÂMETRO = CLASSE CRIADORA DE COMPONENTE
-customElements.define(`card-aluno`, card)
+customElements.define(`card-aluno`, Card)
